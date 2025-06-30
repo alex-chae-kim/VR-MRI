ee2d1f3516765ebce6cc7e746f2b3822c0adfbcc
alex-chae-kim <alexk2k05@gmail.com>
2025-06-30 13:06:28 -0400

    Added scan and scan manager

---
+++ Assets/Scripts/Scan.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Scan
+{
+    public string name;
+    public string clipName;
+    public string announcement;
+    public float durationSeconds;
+    public bool allowUserMusic;
+}
