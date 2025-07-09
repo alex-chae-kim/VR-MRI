f6cd8f21d4cbfa586c66b019221778a540536d96
alex-chae-kim <alexk2k05@gmail.com>
2025-07-08 21:07:06 -0400

    Renamed stages + added stage 2 script

---
+++ Assets/Scripts/Stage_2.cs
@@ -0,0 +1,22 @@
+using Unity.VRTemplate;
+using UnityEngine;
+
+public class Stage_2 : StepManager
+{
+    public GameObject turnOffWindow;
+    public GameObject turnOnWindow;
+    public GameObject movement;
+    public Door_Hinge_Manager doorHingeManager;
+
+    public override void OnLastContinue()
+    {
+        Debug.Log("Stage 2: OnLastContinue called");
+        if (turnOffWindow != null)
+        {
+            turnOffWindow.SetActive(false);
+        }
+
+        //close door
+        doorHingeManager.OpenDoor();
+    }
+}
