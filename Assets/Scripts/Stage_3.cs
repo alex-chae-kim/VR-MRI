3873c265c0f9db16e74713aaad8c36d5dfb9eb69
alex-chae-kim <alexk2k05@gmail.com>
2025-07-13 22:17:22 -0400

    Teleport to MRI bed with proper orientation regardless of original position

--- Assets/Scripts/Stage_3.cs
+++ Assets/Scripts/Stage_3.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.XR;
+using Unity.XR.CoreUtils;
 
 public class Stage_3 : MonoBehaviour
 {
+    public Transform target;
+    public XROrigin XROrigin;
     public InputGetter InputGetter;
     public GameObject newXROrigin;
     public GameObject oldXROrigin;
     public GameObject stage4;
-    public Animator Animator;
+    public Animator oldAnimator;
+    public Animator newAnimator;
     private float holdTime = 2f;
-    private bool first = false;
+    private bool first = true;
 
     // Update is called once per frame
     void Update()
@@ -32,12 +37,14 @@ public class Stage_3 : MonoBehaviour
 
     private IEnumerator transitionToMRI()
     {
-        Animator.SetTrigger("FadeOut");
+        oldAnimator.SetTrigger("FadeOut");
         yield return new WaitForSeconds(3f);
-        newXROrigin.SetActive(true);
-        oldXROrigin.SetActive(false);
+        XROrigin.MoveCameraToWorldLocation(target.position);
+        XROrigin.MatchOriginUpCameraForward(target.up, target.forward);
+        oldAnimator.SetTrigger("FadeIn");
+        //oldXROrigin.SetActive(false);
+        //newXROrigin.SetActive(true);
         stage4.SetActive(true);
-        Animator.SetTrigger("FadeIn");
         this.gameObject.SetActive(false);
     }
 }
