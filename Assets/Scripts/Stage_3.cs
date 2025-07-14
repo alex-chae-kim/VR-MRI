6d3159be319e6dd2466bf24ee549a3793d5d80b0
alex-chae-kim <alexk2k05@gmail.com>
2025-07-13 20:32:10 -0400

    Added fade in/out animations and transition to stage 4

--- Assets/Scripts/Stage_3.cs
+++ Assets/Scripts/Stage_3.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class Stage_3 : MonoBehaviour
 {
     public InputGetter InputGetter;
+    public GameObject newXROrigin;
+    public GameObject oldXROrigin;
+    public GameObject stage4;
+    public Animator Animator;
     private float holdTime = 2f;
+    private bool first = false;
 
     // Update is called once per frame
     void Update()
@@ -11,14 +17,27 @@ public class Stage_3 : MonoBehaviour
         if (InputGetter.GetLeftTrigger() > 0.5f && InputGetter.GetRightTrigger() > 0.5f )
         {
             holdTime -= Time.deltaTime;
-            if (holdTime <= 0f)
+            if (holdTime <= 0f && first)
             {
+                first = false;
                 Debug.Log("Stage 3 completed!");
+                StartCoroutine(transitionToMRI());
             }
         }
         else
         {
             holdTime = 2f;
         }
+    }
+
+    private IEnumerator transitionToMRI()
+    {
+        Animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(3f);
+        newXROrigin.SetActive(true);
+        oldXROrigin.SetActive(false);
+        stage4.SetActive(true);
+        Animator.SetTrigger("FadeIn");
+        this.gameObject.SetActive(false);
     }
 }
